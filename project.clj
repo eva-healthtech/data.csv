174a5208ac4904bdde2cc5dea0cac43393f3bbea
Mark Watts <mark.watts@microtest.co.uk>
2021-02-08 10:11:40 +0000

    Convert project to Leiningen

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject blackwaterpark/data.csv "0.1.0-SNAPSHOT"
+  :description "A Clojure library for reading and writing comma separated value (csv) files (Eva fork)"
+  :url "https://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "https://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]  
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
