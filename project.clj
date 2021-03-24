d2bb4520facd6277b18b256d38d1b00b374fed8f
Mark Watts <mark.watts@microtest.co.uk>
2021-03-24 16:23:01 +0000

    Update version to 1.1.2

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject blackwaterpark/data.csv "1.0.0"
+(defproject blackwaterpark/data.csv "1.1.2"
   :description "A Clojure library for reading and writing comma separated value (csv) files (Eva fork)"
   :url "https://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "https://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]  
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/test.check "1.1.0"]]  
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
